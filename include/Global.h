57e2915c59155e063addf20279f63bf3a7a73680
DerZ-apcs <ducthinhshyn1510@gmail.com>
2025-06-01 13:43:41 +0700

    add global, setting

---
+++ include/Global.h
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+#include "../include/ResourceManager.h"
+#include "../include/Setting.h"
+#include "../include/Singleton.h"
+#include <map>
+
+extern ResourceManager& RESOURCE_MANAGER;
+extern Setting& SETTING;
+
+#endif
---
+++ src/Global.cpp
@@ -0,0 +1,4 @@
+#include "../include/Global.h"
+
+ResourceManager& RESOURCE_MANAGER = Singleton<ResourceManager>::getInstance();
+Setting& SETTING = Singleton<Setting>::getInstance();
