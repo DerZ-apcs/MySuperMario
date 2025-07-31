2aefbe983fe609643748411b00e1aaab602b5713
ththichcode <nth3108200606@gmail.com>
2025-07-31 08:39:38 +0700

    Add Boss Class

---
+++ include/Boss.h
@@ -0,0 +1,32 @@
+#ifndef BOSS_H
+#define BOSS_H
+
+#include "Enemy.h"
+#include "CollisionInfo.h"
+#include <vector>
+
+class Boss : public Enemy {
+protected:
+    int maxHp;
+    int currentHp;
+    bool vulnerable;
+    float stateTimer;
+
+public:
+    Boss(Vector2 pos, Vector2 size, Texture2D initialTexture, int hp);
+    virtual ~Boss() = default;
+    virtual void takeDamage(int amount);
+    virtual void die();
+
+    virtual void onDeath();
+
+    virtual void onHit();
+
+    void Update() override;
+
+    virtual void updateBehavior() = 0;
+
+    virtual void applyPhysics();
+};
+,
+#endif
